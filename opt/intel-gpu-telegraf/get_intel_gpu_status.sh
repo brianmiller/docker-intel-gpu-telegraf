e4199205f43a46f83348160102bbba54e0265a9f
brianmiller <brian@phospher.com>
2020-06-03 09:22:48 -0700

    Update get_intel_gpu_status.sh

--- opt/intel-gpu-telegraf/get_intel_gpu_status.sh
+++ opt/intel-gpu-telegraf/get_intel_gpu_status.sh
@@ -6,7 +6,8 @@
 JSON=$(/usr/bin/timeout -k 3 3 /usr/bin/intel_gpu_top -J)
 VIDEO_UTIL=$(echo "$JSON"|grep Video/0 -A 1|tail -1|grep busy|cut -d ":" -f2|cut -d "," -f1|cut -d " " -f2)
 
-
+#Spit out something telegraf can work with
 echo "[{\"time\": `date +%s`, \"intel_gpu_util\": "$VIDEO_UTIL"}]"
 
+#Exit cleanly
 exit 0
